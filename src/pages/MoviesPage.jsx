c7b45c4036fe670d2c793536577e7f8ed09eafee
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2022-02-10 16:29:24 +0200

    query

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,3 +1,28 @@
+import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
 export const MoviesPage = () => {
-  return <div>It's MoviesPage</div>;
+  const [searchParams, setsearchParams] = useSearchParams();
+
+  // сделать запрос
+  const query = searchParams.get('query');
+
+  useEffect(() => {
+    if (query) {
+      console.log('делаем риквест');
+    }
+  }, [query]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.currentTarget.elements.query.value);
+    setsearchParams({ query: e.currentTarget.elements.query.value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <input type="text" name="query" />
+      <button type="submit">Search query</button>
+    </form>
+  );
 };
