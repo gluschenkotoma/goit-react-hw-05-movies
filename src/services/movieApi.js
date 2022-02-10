ea74802464a874e8c4975b86e5aafa05312fb7cd
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2022-02-10 16:05:03 +0200

    fetch

--- src/services/movieApi.js
+++ src/services/movieApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_KEY = 'c3b1e4ac20f5db884f8c75b276d34d38';
-axios.defaults.baseURL = 'https://www.themoviedb.org';
+axios.defaults.baseURL = 'https://api.themoviedb.org';
 
 export const getMovies = async () => {
   const response = await axios.get(`/3/trending/movie/day?api_key=${API_KEY}`);
