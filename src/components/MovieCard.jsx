f34bbc4915e100a85effad08e300ca9fad7a2cbf
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2022-02-11 14:00:24 +0200

    fetchMoviesById

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,67 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+export const MovieCard = ({ movie }) => {
+  const {
+    id,
+    poster_path,
+    overview,
+    genres,
+    vote_average,
+    title,
+    release_date,
+  } = movie;
+
+  let genresName = [];
+
+  function getGenresByName() {
+    return genres.map(genre => genresName.push(genre.name));
+  }
+
+  getGenresByName();
+
+  return (
+    <div>
+      {movie && (
+        <div>
+          {' '}
+          <div>
+            <div>
+              <img
+                src={`https://image.tmdb.org/t/p/w342${poster_path}`}
+                alt={title}
+              />
+            </div>
+
+            <div>
+              <h1>
+                {title} ({release_date?.slice(0, 4)})
+              </h1>
+              <p>User Score: {vote_average * 10} %</p>
+              <h2>Overview</h2>
+              <p>{overview}</p>
+              <h2>Genres</h2>
+              <p>{genresName.join(', ')}</p>
+            </div>
+          </div>
+          <div>
+            <h2>Additional information</h2>
+            <ul>
+              <li>
+                <NavLink to={`/movies/${id}/cast`}>CAST</NavLink>
+              </li>
+              <li>
+                <NavLink to={`/movies/${id}/reviews`}>REWIEVS</NavLink>
+              </li>
+            </ul>
+            <Suspense fallback="">
+              <Outlet />
+            </Suspense>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,35 +1,14 @@
-import { Suspense } from 'react';
 import { useState, useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
-import { NavLink, useParams } from 'react-router-dom';
-
+import { useParams } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
-
 import { getMoviesById } from 'services/movieApi';
 import { Loader } from 'components/Loader';
+import { MovieCard } from 'components/MovieCard';
 
 export const MovieDetailsPage = () => {
   const [loading, setLoading] = useState(false);
   const [movie, setMovie] = useState(null);
   const movieId = useParams();
-
-  const {
-    id,
-    poster_path,
-    overview,
-    genres,
-    vote_average,
-    title,
-    release_date,
-  } = movie;
-
-  let genresName = [];
-
-  function getGenresByName() {
-    return genres.map(genre => genresName.push(genre.name));
-  }
-
-  getGenresByName();
 
   useEffect(() => {
     async function fetchMoviesById() {
@@ -50,44 +29,7 @@ export const MovieDetailsPage = () => {
     <div>
       {loading && <Loader />}
 
-      {movie && (
-        <div>
-          {' '}
-          <div>
-            <div>
-              <img
-                src={`https://image.tmdb.org/t/p/w342${poster_path}`}
-                alt={title}
-              />
-            </div>
-
-            <div>
-              <h1>
-                {title} ({release_date?.slice(0, 4)})
-              </h1>
-              <p>User Score: {vote_average * 10} %</p>
-              <h2>Overview</h2>
-              <p>{overview}</p>
-              <h2>Genres</h2>
-              <p>{genresName.join(', ')}</p>
-            </div>
-          </div>
-          <div>
-            <h2>Additional information</h2>
-            <ul>
-              <li>
-                <NavLink to={`/movies/${id}/cast`}>CAST</NavLink>
-              </li>
-              <li>
-                <NavLink to={`/movies/${id}/reviews`}>REWIEVS</NavLink>
-              </li>
-            </ul>
-            <Suspense fallback="">
-              <Outlet />
-            </Suspense>
-          </div>
-        </div>
-      )}
+      {movie && <MovieCard movie={movie} />}
     </div>
   );
 };
